6b4bc0950bc17afd960e8fbe9d12836f27605271
Simon-Rabich <simonraviz1997@gmail.com>
2022-09-06 16:59:05 +0300

    added testes

--- cypress.config.js
+++ cypress.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
     password: "admin",
     bad_user: 'aaabbbccc',
     bad_pass: '1234567',
-  }
+    home_page_url: 'http://automationpractice.com/index.php',
+    url_after_logout: 'http://automationpractice.com/index.php?controller=authentication&back=my-account',
+    url_after_login: 'http://automationpractice.com/index.php?controller=my-account',
+  },
 });
